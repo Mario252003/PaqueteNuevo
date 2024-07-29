5eb41ab08d83f1cb1e235b5dd663b3f7a5c962d9
Mario252003 <ux21ii039@ux.edu.mx>
2024-07-29 02:40:57 -0600

    new
    new

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 DESCRIPTION = 'Mi primer paquete de Python'
 LONG_DESCRIPTION = 'Mi primer paquete de Python con una descripción ligeramente más larga'
 
