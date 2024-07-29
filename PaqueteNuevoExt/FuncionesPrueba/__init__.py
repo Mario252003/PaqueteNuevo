b447437060b88327e18f94557d72ef33dc8853a6
Mario252003 <ux21ii039@ux.edu.mx>
2024-07-29 02:23:17 -0600

    cambio
    dvsa

--- PaqueteNuevoExt/FuncionesPrueba/__init__.py
+++ PaqueteNuevoExt/FuncionesPrueba/__init__.py
@@ -1,3 +1,3 @@
-from .FuncionesMultivariables import rastrigin, ackley, himmelblau, sphere, rosenbrock,  beale, goldstein, booth, bunkin, matyas, levi, threehumpcamel,easom,crossintray, eggholder, holdertable, mccormick, schaffer2, schaffer_n4,  styblinskitang, shekel
+from FuncionesMultivariables import rastrigin, ackley, himmelblau, sphere, rosenbrock,  beale, goldstein, booth, bunkin, matyas, levi, threehumpcamel,easom,crossintray, eggholder, holdertable, mccormick, schaffer2, schaffer_n4,  styblinskitang, shekel
 
-from .FuncionesUnivariables import f1, f2, f3, f4
+from FuncionesUnivariables import f1, f2, f3, f4
--- PaqueteNuevoExt/__init__.py
+++ PaqueteNuevoExt/__init__.py
@@ -1,16 +1,16 @@
-from .MetodosMultivariables.caminataAleatoria import caminata_aleatoria
-from .MetodosMultivariables.cauchy import cauchy
-from .MetodosMultivariables.fletcherReeves import fletcherReeves
-from .MetodosMultivariables.hookeJeeves import hooke_jeeves
-from .MetodosMultivariables.nelderMeadSimplex import nelder_mead
-from .MetodosMultivariables.newton import newton
+from MetodosMultivariables.caminataAleatoria import caminata_aleatoria
+from MetodosMultivariables.cauchy import cauchy
+from MetodosMultivariables.fletcherReeves import fletcherReeves
+from MetodosMultivariables.hookeJeeves import hooke_jeeves
+from MetodosMultivariables.nelderMeadSimplex import nelder_mead
+from MetodosMultivariables.newton import newton
 
-from .MetodosUnivariables.Fibonacci import fibonacci_search
-from .MetodosUnivariables.biseccion import biseccion
-from .MetodosUnivariables.BusquedaDorada import busquedaDorada
-from .MetodosUnivariables.IntervalHalving import interval_halving_method
-from .MetodosUnivariables.Secante import secante
-from .MetodosUnivariables.NewtonRaph import newton_raphson
+from MetodosUnivariables.Fibonacci import fibonacci_search
+from MetodosUnivariables.biseccion import biseccion
+from MetodosUnivariables.BusquedaDorada import busquedaDorada
+from MetodosUnivariables.IntervalHalving import interval_halving_method
+from MetodosUnivariables.Secante import secante
+from MetodosUnivariables.NewtonRaph import newton_raphson
 
-from .FuncionesPrueba.FuncionesMultivariables import rastrigin, ackley, himmelblau, sphere, rosenbrock,  beale, goldstein, booth, bunkin, matyas, levi, threehumpcamel,easom,crossintray, eggholder, holdertable, mccormick, schaffer2, schaffer_n4,  styblinskitang, shekel  
-from .FuncionesPrueba.FuncionesUnivariables import f1, f2, f3, f4
+from FuncionesPrueba.FuncionesMultivariables import rastrigin, ackley, himmelblau, sphere, rosenbrock,  beale, goldstein, booth, bunkin, matyas, levi, threehumpcamel,easom,crossintray, eggholder, holdertable, mccormick, schaffer2, schaffer_n4,  styblinskitang, shekel  
+from FuncionesPrueba.FuncionesUnivariables import f1, f2, f3, f4
