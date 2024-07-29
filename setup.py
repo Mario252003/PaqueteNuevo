62a4fac5b134e5491f4f25e6f1834f37476d23e4
Mario252003 <ux21ii039@ux.edu.mx>
2024-07-29 02:26:31 -0600

    f
    d

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
        # el nombre debe coincidir con el nombre de la carpeta 	  
        #'modulomuysimple'
         name="PaqueteNuevoExt", 
-        version="0.1",
+        version="0.3",
         author="Mario Zepeda",
         author_email="<marioraul.zepeda@hotmail.com>",
         description=DESCRIPTION,
